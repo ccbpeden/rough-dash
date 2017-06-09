e341e7c28f52b3fbfe8e21b568c9d6a0038b6a59
Charles Peden <charles@pancakecreative.com>
2017-06-09 14:00:24 -0700

    refactor editor component to use child routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,6 +9,14 @@ import { HomeComponent } from './home/home.component';
 import { EditorComponent } from './editor/editor.component';
 import { DashGridDetailComponent } from './dash-grid-detail/dash-grid-detail.component';
 import { DashRowDetailComponent } from './dash-row-detail/dash-row-detail.component';
+import { EditorDetail360PhotosComponent } from './editor-detail-360-photos/editor-detail-360-photos.component';
+import { EditorDetailPromoBannerComponent } from './editor-detail-promo-banner/editor-detail-promo-banner.component';
+import { EditorDetailMenuLinksComponent } from './editor-detail-menu-links/editor-detail-menu-links.component';
+import { EditorDetailVideoGalleryComponent } from './editor-detail-video-gallery/editor-detail-video-gallery.component';
+import { EditorDetailPhotoGalleryComponent } from './editor-detail-photo-gallery/editor-detail-photo-gallery.component';
+import { EditorDetailMapDirectionsComponent } from './editor-detail-map-directions/editor-detail-map-directions.component';
+import { EditorDetailInfoLinksComponent } from './editor-detail-info-links/editor-detail-info-links.component';
+import { EditorDetailAnalyticsComponent } from './editor-detail-analytics/editor-detail-analytics.component';
 import { BillingDetailInvoicesComponent } from './billing-detail-invoices/billing-detail-invoices.component';
 import { BillingDetailAlertsComponent } from './billing-detail-alerts/billing-detail-alerts.component';
 import { BillingDetailPaymentMethodComponent } from './billing-detail-payment-method/billing-detail-payment-method.component';
@@ -42,7 +50,18 @@ const appRoutes: Routes = [
   },
   {
   path: 'editor',
-  component: EditorComponent
+  component: EditorComponent,
+  children: [
+    { path: '', redirectTo: '360-photos', pathMatch: 'full' },
+    { path: '360-photos', component: EditorDetail360PhotosComponent },
+    { path: 'analytics', component: EditorDetailAnalyticsComponent },
+    { path: 'info-links', component: EditorDetailInfoLinksComponent },
+    { path: 'map-directions', component: EditorDetailMapDirectionsComponent },
+    { path: 'menu-links', component: EditorDetailMenuLinksComponent },
+    { path: 'photo-gallery', component: EditorDetailPhotoGalleryComponent },
+    { path: 'promo-banner', component: EditorDetailPromoBannerComponent },
+    { path: 'video-gallery', component: EditorDetailVideoGalleryComponent },
+  ]
   },
 ];
 
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editor',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
   detailToDisplay = "360-photos";
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
