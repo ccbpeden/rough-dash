1d955b8a2d643d69981011721e46d6c6e669f076
Charles Peden <charles@pancakecreative.com>
2017-06-09 10:14:11 -0700

    rough out grid view

--- src/app/dash-grid-detail/dash-grid-detail.component.ts
+++ src/app/dash-grid-detail/dash-grid-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dash-grid-detail',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashGridDetailComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router) { }
+  tourName = "Lovely Wedding Venue";
   ngOnInit() {
   }
-
+  goToEditor(){
+    this.router.navigate(['editor']);
+  }
 }
