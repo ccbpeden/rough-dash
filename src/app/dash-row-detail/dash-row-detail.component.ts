ab421e5f1872ac1f80b3d4b0e37b4e5ed7a4d700
Charles Peden <charles@pancakecreative.com>
2017-06-09 10:05:03 -0700

    complete rough out of row design

--- src/app/dash-row-detail/dash-row-detail.component.ts
+++ src/app/dash-row-detail/dash-row-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dash-row-detail',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashRowDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  goToEditor(){
+    this.router.navigate(['editor']);
+  }
 }
