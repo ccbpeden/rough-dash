f002f019db2345401ef1dcb6705388b6b8b9495f
Charles Peden <charles@pancakecreative.com>
2017-06-09 15:14:39 -0700

    rough out account component

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
-
+  countries = ["United States", "Unwashed Swine"];
+  imgSrc="/assets/images/generic-user.jpg";
   constructor() { }
 
   ngOnInit() {
