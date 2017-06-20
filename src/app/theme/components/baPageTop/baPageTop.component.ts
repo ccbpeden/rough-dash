511314c47dcf8003d1a6f4ac6d9e5aee2523d6c8
Charles Peden <charles@pancakecreative.com>
2017-06-20 15:17:07 -0700

    add select list to top component

--- src/app/theme/components/baPageTop/baPageTop.component.ts
+++ src/app/theme/components/baPageTop/baPageTop.component.ts
@@ -12,11 +12,37 @@ export class BaPageTop {
 
   public isScrolled:boolean = false;
   public isMenuCollapsed:boolean = false;
+  public query = '';
+  public trueTours = [ "Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus",
+                       "Belgium","Bosnia & Herzegovina","Bulgaria","Croatia","Cyprus",
+                       "Czech Republic","Denmark","Estonia","Finland","France","Georgia",
+                       "Germany","Greece","Hungary","Iceland","Ireland","Italy","Kosovo",
+                       "Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta",
+                       "Moldova","Monaco","Montenegro","Netherlands","Norway","Poland",
+                       "Portugal","Romania","Russia","San Marino","Serbia","Slovakia","Slovenia",
+                       "Spain","Sweden","Switzerland","Turkey","Ukraine","United Kingdom","Vatican City"];
+  public filteredList = [];
+  public elementRef;
 
   constructor(private _state:GlobalState, private authService: AuthService, private router: Router) {
     this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
       this.isMenuCollapsed = isCollapsed;
     });
+  }
+
+  filter() {
+    if (this.query !== ""){
+        this.filteredList = this.trueTours.filter(function(el){
+            return el.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
+        }.bind(this));
+    } else {
+        this.filteredList = [];
+    }
+  }
+
+  select(item){
+      this.query = item;
+      this.filteredList = [];
   }
 
   public toggleMenu() {
