0676246663f11356eaba69284f74142090d6a6e6
Charles Peden <charles@pancakecreative.com>
2017-06-21 11:43:02 -0700

    style sidebar menu and header for editor component

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { LoginModalComponent } from '../login-modal/login-modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from '../providers/auth.service';
+import { GlobalState } from '../global.state';
 
 @Component({
   selector: 'app-editor',
@@ -13,13 +14,26 @@ import { AuthService } from '../providers/auth.service';
 export class EditorComponent implements OnInit {
   viewAsList = true;
   viewAsGrid = false;
-  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService, private modalService: NgbModal,) {
+  public isMenuCollapsed:boolean = false;
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService, private modalService: NgbModal, private _state:GlobalState,) {
     this.authService.user.subscribe(
       (auth) => {
         if(auth == null){
           const activeModal = this.modalService.open(LoginModalComponent, {size: 'sm', backdrop: 'static'});
         }
       });
+      this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
+        this.isMenuCollapsed = isCollapsed;
+        console.log(isCollapsed);
+      });
+   }
+
+   checkCollapse(){
+     if (this.isMenuCollapsed){
+       return "sidebar-collapsed";
+     } else {
+       return "sidebar";
+     }
    }
 
   ngOnInit() {
