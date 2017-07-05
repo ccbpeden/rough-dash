625d70ce5f325b26201963ef72a8f4cba18b1626
Charles Peden <charles@pancakecreative.com>
2017-07-05 15:36:18 -0700

    add all services to pages.module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,7 +4,6 @@ import { CommonModule }  from '@angular/common';
 import { routing }       from './pages.routing';
 import { NgaModule } from '../theme/nga.module';
 import { AppTranslationModule } from '../app.translation.module';
-import { AuthService } from '../providers/auth.service';
 import { Pages } from './pages.component';
 import { EditorComponent } from '../editor/editor.component';
 import { EditRowDetailComponent } from '../edit-row-detail/edit-row-detail.component';
@@ -16,10 +15,27 @@ import { PasswordDetailComponent } from '../password-detail/password-detail.comp
 import { EmailDetailComponent } from '../email-detail/email-detail.component';
 import { ProfileDetailComponent } from '../profile-detail/profile-detail.component';
 
+import { AuthService } from '../providers/auth.service';
+import { UserService } from '../providers/user.service';
+import { ThreeSixtyPhotoService } from '../providers/360photo.service';
+import { AddressService } from '../providers/address.service';
+import { BannerService } from '../providers/banner.service';
+import { CardService } from '../providers/card.service';
+import { CategoryService } from '../providers/category.service';
+import { GalleryService } from '../providers/gallery.service';
+import { InfoLinkService } from '../providers/infoLink.service';
+import { MapService } from '../providers/map.service';
+import { MenuLinkService } from '../providers/menuLink.service';
+import { PartnerListService } from '../providers/partnerList.service';
+import { PhotoService } from '../providers/photo.service';
+import { ProfileService } from '../providers/profile.service';
+import { TourService } from '../providers/tour.service';
+import { VideoService } from '../providers/video.service';
+
 @NgModule({
   imports: [CommonModule, AppTranslationModule, NgaModule, routing],
   declarations: [Pages, EditorComponent, EditRowDetailComponent, EditGridDetailComponent, AccountComponent, InvoiceDetailComponent, BillingDetailComponent, PasswordDetailComponent, EmailDetailComponent, ProfileDetailComponent],
-  providers: [AuthService],
+  providers: [AuthService, UserService, ThreeSixtyPhotoService, AddressService, BannerService, CardService, CategoryService, GalleryService, InfoLinkService, MapService, MenuLinkService, PartnerListService, PhotoService, ProfileService, TourService, VideoService],
 })
 export class PagesModule {
 }
