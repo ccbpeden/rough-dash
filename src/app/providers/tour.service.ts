b812303bdd6c1130f8c9103cc860a1eac1e7700e
Charles Peden <charles@pancakecreative.com>
2017-07-05 09:20:54 -0700

    create tour service

---
+++ src/app/providers/tour.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Tour } from '../models/tour.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class UserService {
+  private trueTours: FirebaseListObservable<any[]>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.trueTours = db.list('trueTours');
+  }
+
+  getUsers() {
+    return this.trueTours;
+  }
+
+  newTrueTour(user: User) {
+    this.trueTours.push(user);
+  }
+
+  getTrueTourByKey(key: string){
+    return this.db.list('/trueTours' + key);
+  }
+
+  getTrueTourByUid(uid: string) {
+    return this.db.list('/trueTours', {
+      query: {
+        orderByChild: 'uid',
+        equalTo: uid
+      }
+    })
+  }
+
+  editTrueTour(key: string, values: Object = {}) {
+    let trueTourInFirebase = this.getTrueTourByKey(key);
+    if(!trueTourInFirebase){
+      return null;
+    }
+    trueTourInFirebase.update(key, {values});
+    return trueTourInFirebase;
+  }
+
+  deleteTrueTour(key: string) {
+    let trueTourInFirebase = this.getTrueTourByKey(key);
+    trueTourInFirebase.remove();
+  }
+
+}
