25a7656c9525f0590a5900aed1bea049bf88ecc1
Charles Peden <charles@pancakecreative.com>
2017-07-05 14:09:21 -0700

    create photo service

---
+++ src/app/providers/photo.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Photo } from '../models/photo.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class PhotoService {
+  private photos: FirebaseListObservable<any[]>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.photos = db.list('/photos');
+  }
+
+  getGalleries() {
+    return this.photos;
+  }
+
+  newPhoto(photo: Photo) {
+    this.photos.push(photo);
+  }
+
+  getPhotoByKey(key: string){
+    return this.db.list('/photos' + key);
+  }
+
+  getPhotoByGalleryReferenceID(gID: string) {
+    return this.db.list('/photos', {
+      query: {
+        orderByChild: 'galleryReferenceId',
+        equalTo: gID
+      }
+    })
+  }
+
+  editPhoto(key: string, values: Object = {}) {
+    let photoInFirebase = this.getPhotoByKey(key);
+    if(!photoInFirebase){
+      return null;
+    }
+    photoInFirebase.update(key, {values});
+    return photoInFirebase;
+  }
+
+  deletePhoto(key: string) {
+    let photoInFirebase = this.getPhotoByKey(key);
+    photoInFirebase.remove();
+  }
+
+}
