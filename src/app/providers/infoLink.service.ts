0258d49835c7ec921a33fa9249ca7a536dfaf6cc
Charles Peden <charles@pancakecreative.com>
2017-07-05 14:23:13 -0700

    create infoLink service

---
+++ src/app/providers/infoLink.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { InfoLink } from '../models/infoLink.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class InfoLinkListService {
+  private infoLinks: FirebaseListObservable<any[]>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.infoLinks = db.list('/infoLinks');
+  }
+
+  getInfoLinkLists() {
+    return this.infoLinks;
+  }
+
+  newInfoLinkList(infoLink: InfoLink) {
+    this.infoLinks.push(infoLink);
+  }
+
+  getInfoLinkListByKey(key: string){
+    return this.db.list('/infoLinks' + key);
+  }
+
+  getInfoLinkListByTTID(tTID: string) {
+    return this.db.list('/infoLinks', {
+      query: {
+        orderByChild: 'tTReferenceID',
+        equalTo: tTID,
+      }
+    })
+  }
+
+  editInfoLinkList(key: string, values: Object = {}) {
+    let infoLinkInFirebase = this.getInfoLinkListByKey(key);
+    if(!infoLinkInFirebase){
+      return null;
+    }
+    infoLinkInFirebase.update(key, {values});
+    return infoLinkInFirebase;
+  }
+
+  deleteInfoLinkList(key: string) {
+    let infoLinkInFirebase = this.getInfoLinkListByKey(key);
+    infoLinkInFirebase.remove();
+  }
+  
+}
