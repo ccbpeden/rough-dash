c062341c38b1ac9c99159038f15f67a514b64ea4
Charles Peden <charles@pancakecreative.com>
2017-07-06 13:27:24 -0700

    wire up password page for receiving data

--- src/app/password-detail/password-detail.component.ts
+++ src/app/password-detail/password-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../providers/user.service';
+import { User } from '../models/user.model';
+import { AuthService } from '../providers/auth.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-password-detail',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-detail.component.scss']
 })
 export class PasswordDetailComponent implements OnInit {
+  private uid;
+  private user: User;
+  private userKey;
 
-  constructor() { }
+
+  constructor(private authService: AuthService, private userService: UserService, private db: AngularFireDatabase) {
+    this.authService.user.subscribe((auth) => {
+      this.uid = auth.uid;
+    });
+  }
 
   ngOnInit() {
+    this.userService.getUserByUid(this.uid).subscribe((user) => {
+      if(user.length > 0)
+      {
+        this.user = user[0];
+        this.userKey = user[0].$key;
+        console.log(this.user);
+      };
+    });
+  }
+
+  changePassword(currentPwd, newPwd, confirmPwd){
+    // if(this.user) {
+    //   if(currentPwd === this.user.password && newPwd === confirmPwd) {
+        // this.userService.editUser(userKey, {password: newPwd});
+    //   }
+    // }
   }
 
 }
