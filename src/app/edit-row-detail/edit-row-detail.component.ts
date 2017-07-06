6e1a6579c465f1fa410ea8d2f171adb67fa2e8de
Charles Peden <charles@pancakecreative.com>
2017-07-06 16:43:06 -0700

    add data to row detail

--- src/app/edit-row-detail/edit-row-detail.component.ts
+++ src/app/edit-row-detail/edit-row-detail.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../providers/auth.service';
+import { User } from '../models/user.model';
+import { UserService } from '../providers/user.service';
+import { TourService } from '../providers/tour.service';
+import { Tour } from '../models/tour.model';
+import { GalleryService } from '../providers/gallery.service';
+import { GlobalState } from '../global.state';
 
 @Component({
   selector: 'app-edit-row-detail',
@@ -7,12 +14,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./edit-row-detail.component.scss']
 })
 export class EditRowDetailComponent implements OnInit {
-  tourName = "Lovely Wedding Venue";
-  constructor(private router: Router) { }
+  private uid;
+  private user: User;
+  private userKey;
+  private trueTours: Tour[];
+
+  constructor(private router: Router, private authService: AuthService, private userService: UserService, private tourService: TourService, private galleryService: GalleryService, private globalState: GlobalState) {
+    this.authService.user.subscribe((auth) => {
+      this.uid = auth.uid;
+    });
+  }
 
   ngOnInit() {
+    this.userService.getUserByUid(this.uid).subscribe((user) => {
+      if(user.length > 0)
+      {
+        this.user = user[0];
+        this.userKey = user[0].$key;
+      };
+    });
+    this.tourService.getTrueTourByUid(this.userKey).subscribe((returnedTours) => {
+      if(returnedTours.length > 0)
+      {
+        this.trueTours = returnedTours;
+        console.log(this.trueTours);
+      };
+    });
   }
+
   goToDashboard(){
     this.router.navigate(['pages']);
+  }
+
+  setTourKey(key){
+    this.tourService.setCurrentTourKey(key);
   }
 }
