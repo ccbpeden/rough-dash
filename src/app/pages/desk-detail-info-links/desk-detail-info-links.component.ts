6a124e47b14994279ef0637aab01b5b807c3a017
Charles Peden <charles@pancakecreative.com>
2017-07-07 12:49:38 -0700

    hook up info links to data

--- src/app/pages/desk-detail-info-links/desk-detail-info-links.component.ts
+++ src/app/pages/desk-detail-info-links/desk-detail-info-links.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../providers/user.service';
+import { User } from '../../models/user.model';
+import { AuthService } from '../../providers/auth.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { TourService } from '../../providers/tour.service';
+import { InfoLinkService } from '../../providers/infoLink.service';
+
 
 @Component({
   selector: 'app-desk-detail-info-links',
@@ -6,10 +14,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desk-detail-info-links.component.scss']
 })
 export class DeskDetailInfoLinksComponent implements OnInit {
+  private uid;
+  private user: User;
+  private userKey;
+  private tourKey;
+  private infoLink;
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService, private userService: UserService, private tourService: TourService, private infoLinkService: InfoLinkService) {
+    this.authService.user.subscribe((auth) => {
+      this.uid = auth.uid;
+    });
+  }
 
   ngOnInit() {
+    this.tourKey = this.tourService.returnCurrentTourKey();
+    if (this.tourKey) {
+      this.infoLinkService.getInfoLinkListByTTID(this.tourKey).subscribe((links) => {
+        this.infoLink = links[0];
+        console.log(this.infoLink);
+      });
+    }
   }
 
 }
