44759798d2d2482423873b4d51ddfe0fbae8db0a
Charles Peden <ccbpeden@warpmail.net>
2017-07-13 11:36:23 -0700

    refactor editor component to include child logic

--- src/app/pages/editor/editor.component.ts
+++ src/app/pages/editor/editor.component.ts
@@ -6,6 +6,11 @@ import { LoginModalComponent } from '../login-modal/login-modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from '../../providers/auth.service';
 import { GlobalState } from '../../global.state';
+import { User } from '../../models/user.model';
+import { UserService } from '../../providers/user.service';
+import { TourService } from '../../providers/tour.service';
+import { Tour } from '../../models/tour.model';
+import { GalleryService } from '../../providers/gallery.service';
 
 @Component({
   selector: 'app-editor',
@@ -13,13 +18,19 @@ import { GlobalState } from '../../global.state';
   styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent implements OnInit, OnDestroy {
-  private subscription: ISubscription;
+  private authSubscription: ISubscription;
+  private userSubscription: ISubscription;
+  private tourSubscription: ISubscription;
+  private uid;
+  private user: User;
+  private userKey;
+  public trueTours: Tour[];
   menuList:any;
   selected:any;
   viewAsList = true;
   viewAsGrid = false;
   public isMenuCollapsed:boolean = false;
-  constructor(private router: Router, private route: ActivatedRoute, private _state:GlobalState, private authService: AuthService, private modalService: NgbModal) {
+  constructor(private router: Router, private route: ActivatedRoute, private _state:GlobalState, private authService: AuthService, private modalService: NgbModal, private userService: UserService, private tourService: TourService, private galleryService: GalleryService ) {
       this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
         this.isMenuCollapsed = isCollapsed;
         console.log(isCollapsed);
@@ -38,13 +49,12 @@ export class EditorComponent implements OnInit, OnDestroy {
         "subMenu" : ["TrueTour1", "TrueTour2", "TrueTour3"]
       },
     ]
-    this.subscription = this.authService.user.subscribe(
+    this.authSubscription = this.authService.user.subscribe(
       (auth) => {
         if(auth == null){
           const activeModal = this.modalService.open(LoginModalComponent, {size: 'sm', backdrop: 'static', windowClass: 'login-modal'});
-          console.log(auth);
         } else {
-          console.log(auth);
+          this.uid = auth.uid;
         }
       });
    }
@@ -75,9 +85,29 @@ export class EditorComponent implements OnInit, OnDestroy {
    }
 
   ngOnInit() {
+    this.userSubscription = this.userService.getUserByUid(this.uid).subscribe((user) => {
+      if(user.length > 0)
+      {
+        this.user = user[0];
+        this.userKey = user[0].$key;
+      };
+    });
+    this.tourSubscription = this.tourService.getTrueTourByUid(this.userKey).subscribe((returnedTours) => {
+      if(returnedTours.length > 0)
+      {
+        this.trueTours = returnedTours;
+        console.log(this.trueTours);
+      };
+    });
+  }
+
+  setTourKey(key){
+    this.tourService.setCurrentTourKey(key);
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.authSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+    this.tourSubscription.unsubscribe();
   }
 }
